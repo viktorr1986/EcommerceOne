05394d4789eec175de0f5acb2a7446f17e9de98b
unknown <huggovhrg@gmail.com>
2023-08-21 14:55:28 -0500

    se implemento nuevas funcionalidades al proyecto4

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,52 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 //import productos from '../productos'
 import Producto from '../Componentes/Producto.js'
-import axios from 'axios'
+//import axios from 'axios'
+import { listProducts } from '../Actions/productActions.js'
 
 function HomeScreen() {
 
-    const [productos, setProductos] = useState([])
-    
-    useEffect(()=> {
-        async function fetchProductos(){
-            const { data } = await axios.get('/api/products/')
-            setProductos(data)
-        }
-        fetchProductos()
-    }, [])
+    // const [productos, setProductos] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { error, loading, products } = productList
 
-  return (
-    <div>
-        <h1>nuestro delicioso menu</h1>
-        <Row>
+    useEffect(() => {
+        //async function fetchProductos() {
+        //const { data } = await axios.get('/api/products/')
+        //setProductos(data)
+        //}
+        //fetchProductos()
+
+        dispatch(listProducts())
+
+    }, [dispatch])
+
+    return (
+        <div>
+            <h1>nuestro delicioso menu</h1>
             {
-                productos.map(producto => (
-                    <Col key={producto._id} sm={12} md={6} lg={4} xl={3} >
-                        <Producto producto={producto} />
-                    </Col>
-                ))
+                loading ? <div class="text-center">
+                            <div class="spinner-border" role="status">
+                                <span class="visually-hidden">Cargando...</span>
+                            </div>
+                          </div>
+                    : error ? <h3>{error}</h3>
+                        : <Row>
+                            {
+                                products.map(producto => (
+                                    <Col key={producto._id} sm={12} md={6} lg={4} xl={3} >
+                                        <Producto producto={producto} />
+                                    </Col>
+                                ))
+                            }
+                        </Row>
             }
-        </Row>
-    </div>
-  )
+
+        </div>
+    )
 }
 
 export default HomeScreen
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,78 +1,102 @@
 import React, {useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../Componentes/Rating'
 //import products from '../productos'
-import axios from 'axios'
+//import axios from 'axios'
+import { listProductDetails } from '../Actions/productActions'
 
 function ProductScreen() {
 
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product } = productDetails
+
     const { id } = useParams();
-    const [product, setProduct] = useState([])
+    //const [product, setProduct] = useState([])
     
     useEffect(()=> {
-        async function fetchProduct(){
-            const { data } = await axios.get(`/api/products/${id}`)
-            setProduct(data)
-        }
-        fetchProduct()
+
+        dispatch(listProductDetails(id))
+
+        //async function fetchProduct(){
+            //const { data } = await axios.get(`/api/products/${id}`)
+            //setProduct(data)
+        //}
+        //fetchProduct()
     }, [])
 
-    //const { id } = useParams();
+      //const { id } = useParams();
     //const product = products.find((p) => String(p._id) === id);
 
    // const product = products.find((p) => p._id == match.params.id)
   return (
     <div>
       <Link to='/' className='btn btn-light my-3' >Ir Atras</Link>
-      <Row>
-        <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid></Image>
-        </Col>
 
-        <Col md={3}>
-            <ListGroup variant='flush'>
-                <ListGroup.Item>
-                    <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Rating value={product.rating} text={`${product.numReviews} reseñas`} color={'#f8e825'} ></Rating>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    Precio: $ {product.price}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    Descripcion: {product.description}
-                </ListGroup.Item>
-            </ListGroup>
-        </Col>
-
-        <Col md={3}>
-            <Card>
+      {
+        loading ? <div class="text-center">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Cargando...</span>
+                    </div>
+                  </div>
+        : error ? <h3>{error}</h3>
+        : (
+            <Row>
+            <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid></Image>
+            </Col>
+    
+            <Col md={3}>
                 <ListGroup variant='flush'>
                     <ListGroup.Item>
-                        <Row>
-                            <Col>Precio</Col>
-                            <Col><strong>$ {product.price}</strong> </Col>
-                        </Row>
+                        <h3>{product.name}</h3>
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        <Row>
-                            <Col>Estado</Col>
-                            <Col>
-                                {product.countInStock > 0 ? 'En Stock' : 'Agotado'}
-                            </Col>
-                        </Row>
+                        <Rating value={product.rating} text={`${product.numReviews} reseñas`} color={'#f8e825'} ></Rating>
                     </ListGroup.Item>
-
                     <ListGroup.Item>
-                        <Button className='btn-block' disabled={product.countInStock === 0} type='button'>Agregar a la Cesta</Button>
+                        Precio: $ {product.price}
                     </ListGroup.Item>
-
+                    <ListGroup.Item>
+                        Descripcion: {product.description}
+                    </ListGroup.Item>
                 </ListGroup>
-            </Card>
-        </Col>
-      </Row>
+            </Col>
+    
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Precio</Col>
+                                <Col><strong>$ {product.price}</strong> </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Estado</Col>
+                                <Col>
+                                    {product.countInStock > 0 ? 'En Stock' : 'Agotado'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+    
+                        <ListGroup.Item>
+                            <Button className='btn-block' disabled={product.countInStock === 0} type='button'>Agregar a la Cesta</Button>
+                        </ListGroup.Item>
+    
+                    </ListGroup>
+                </Card>
+            </Col>
+          </Row>
+          )
+      }
+
+
+
+
     </div>
   )
 }
